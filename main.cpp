16364cb7cdf92c2f16914dfc4abd1d0b9f2d996e
khlorz <105799772+khlorz@users.noreply.github.com>
2022-07-26 01:09:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "GameWindow.h"
-#include "font.h"
 #include "IconsFontAwesome5.h"
 #include <filesystem>
 #include "ImGuiFunctions.h"
+#include <Windows.h>
 
 int main()
 {
     OGLSet window_helper;
-    if (!window_helper.InitGLFW("Sudoku Game", 507, 785)) {
+    if (!window_helper.InitGLFW("Sudoku Game", 507, 785))
         return -1;
-    }
     window_helper.SetWindowCentered();
     glfwSwapInterval(1); // Enable V-sync
 
@@ -25,7 +24,6 @@ int main()
     imgui_helper.EnableConfigFlags(ImGuiConfigFlags_NavEnableKeyboard);
     imgui_helper.SetTheme(ImGuiThemes_Dark);
 
-    std::string dir = ((std::filesystem::current_path()).string()) + "\\Fonts\\";
     static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
     ImFontConfig icons_config;
     ImFontConfig CustomFont;
@@ -37,33 +35,50 @@ int main()
     icons_config.OversampleV = 2;
 
     ImGuiIO& io = ImGui::GetIO();
-    io.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Custom), sizeof(Custom), 14.0f, &CustomFont);
-    io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 14.0f, &icons_config, icons_ranges);
-    io.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Custom), sizeof(Custom), 17.0f, &CustomFont);
-    io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 17.0f, &icons_config, icons_ranges);
-    io.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Custom), sizeof(Custom), 20.0f, &CustomFont);
-    io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 20.0f, &icons_config, icons_ranges);
-    io.Fonts->AddFontDefault();
+    try {
+        std::string dir = ((std::filesystem::current_path()).string()) + "\\Fonts\\";
+        const char* fontfile_exception = "Required font not loaded! Quicksand-Medium.ttf file not found! Please make sure the required file is inside the Fonts folder.";
+        const char* iconfont_exception = "Required icon font not loaded! fa-regular-400.ttf file not found! Please make sure the required file is inside the Fonts folder.";
+        if (io.Fonts->AddFontFromFileTTF((dir + "Quicksand-Medium.ttf").c_str(), 15.00f) == nullptr)
+            throw std::exception(fontfile_exception);
+        if (io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 15.00f, &icons_config, icons_ranges) == nullptr)
+            throw std::exception(iconfont_exception);
+        if(io.Fonts->AddFontFromFileTTF((dir + "Quicksand-Medium.ttf").c_str(), 18.00f) == nullptr)
+            throw std::exception(fontfile_exception);
+        if(io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 18.0f, &icons_config, icons_ranges) == nullptr)
+            throw std::exception(iconfont_exception);
+        if(io.Fonts->AddFontFromFileTTF((dir + "Quicksand-Medium.ttf").c_str(), 22.00f) == nullptr)
+            throw std::exception(fontfile_exception);
+        if(io.Fonts->AddFontFromFileTTF((dir + FONT_ICON_FILE_NAME_FAR).c_str(), 22.0f, &icons_config, icons_ranges) == nullptr)
+            throw std::exception(iconfont_exception);
+        if(io.Fonts->AddFontDefault() == nullptr)
+            throw std::exception("Internal failure!");
+    }
+    catch (const std::exception& e) {
+        std::cout << e.what() << "\n";
+
+        window_helper.Shutdown();
+        imgui_helper.Shutdown();
+
+        printf("Press any key to exit... Or just click the close button... ");
+        std::cin.get();
+
+        return -1;
+    }
 
     bool show_demo = true;
 
     GameWindow gamewindow_helper;
-    if (!gamewindow_helper.OneTimeInitialize()) {
-        imgui_helper.Shutdown();
-        window_helper.Shutdown();
-        return -1;
-    }
 
-    while (!window_helper.IsWindowClosed()) {
+    FreeConsole();
+
+    while (!window_helper.IsWindowClosed() && !gamewindow_helper.IsWindowClosed()) {
         glfwPollEvents();
         imgui_helper.ImGuiNewFrame();
 
         //ImGui::ShowDemoWindow();
 
         gamewindow_helper.RenderWindow();
-        if (gamewindow_helper.IsWindowClosed()) {
-            glfwSetWindowShouldClose(main_window_object, GLFW_TRUE);
-        }
 
         ImGuiOGL_Render(main_window_object);
     }
